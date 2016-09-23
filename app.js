6c113f713200f593d1a05446f54083a093de6322
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-09-23 09:27:56 -0400

    new project

---
+++ app.js
@@ -0,0 +1,50 @@
+// this simulate opening a PHC crystal in marvel coc
+// given a list of champions and percentages of opening a 2star 3star or 4star
+
+// randomly opening a crystal has a % chance for each type of crystal 2star is 75% 3star is 20% 4star is 5%
+var openCrystal = function() {
+    let crystal = "";
+    let roll = Math.random();
+
+    if (roll > 0.15) {
+        crystal = "2star"
+    } else if (roll <= 0.15 && roll > 0.02) {
+        crystal = "3star"
+
+    } else if (roll <= 0.02) {
+        crystal = "4star"
+    }
+
+    return crystal
+}
+
+// open an ammount of crystals ex:200 crystals
+var openMultiple = function(ammount) {
+    let openedCrystals = [];
+    let twoStars = [];
+    let threeStars = [];
+    let fourStars = [];
+
+    for (var i = 0; i < ammount; i++) {
+        openedCrystals.push(openCrystal());
+    }
+    console.log(openedCrystals);
+    openedCrystals.forEach(function(el) {
+        if (el === "2star") {
+            twoStars.push(el)
+        } else if (el === "3star") {
+            threeStars.push(el)
+        } else if (el === "4star") {
+            fourStars.push(el)
+        }
+    });
+
+    console.log("ammount of 2stars",
+        twoStars.length);
+    console.log("ammount of 3stars",
+        threeStars.length);
+    console.log("ammount of 4stars",
+        fourStars.length);
+
+
+}
